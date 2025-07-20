9382954fb93dcf953b5142e612b7a9c8c9837ad0
22l5d5 <117133223+22l5d5@users.noreply.github.com>
2025-07-20 18:05:58 +0800

    Update 01.cpp

--- HW1/01.cpp
+++ HW1/01.cpp
@@ -7,26 +7,6 @@ int a(int m, int n) {
     return a(m - 1, a(m, n - 1));
 }
 
-int b(int m, int n) {
-    int result = n + 1;
-    while (m > 0 || n > 0) {
-        if (m == 0) {
-            result = n + 1;
-            break;
-        }
-        if (n == 0) {
-            m--;
-            n = 1;
-        } else {
-            m--;
-            int temp = n;
-            n = result - 1;
-            result = temp;
-        }
-    }
-    return result;
-}
-
 int main() {
     int m, n;
     cout << "請輸入 m: ";
@@ -35,7 +15,6 @@ int main() {
     cin >> n;
     try {
         cout << "A(" << m << ", " << n << ") 遞迴結果: " << a(m, n) << endl;
-        cout << "A(" << m << ", " << n << ") 非遞迴結果: " << b(m, n) << endl;
     } catch (const char* msg) {
         cout << msg << endl;
     }
