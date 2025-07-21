0ed5ca8f0ff6bbe2f636747e79541b7191561f1f
22l5d5 <117133223+22l5d5@users.noreply.github.com>
2025-07-21 08:35:20 +0800

    Update 01.cpp

--- HW1/src/01.cpp
+++ HW1/src/01.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int a(int m, int n) {
+    if (m == 0) return n + 1;
+    if (n == 0) return a(m - 1, 1);
+    return a(m - 1, a(m, n - 1));
+}
+
+int main() {
+    int m, n;
+    cout << "請輸入 m: ";
+    cin >> m;
+    cout << "請輸入 n: ";
+    cin >> n;
+    try {
+        cout << "A(" << m << ", " << n << ") 遞迴結果: " << a(m, n) << endl;
+    } catch (const char* msg) {
+        cout << msg << endl;
+    }
+    return 0;
+}
 
